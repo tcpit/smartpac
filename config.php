2cfba3245d4064ace0e7c382a04ced0a5aea00d1
TCP <tcpit@users.noreply.github.com>
2016-05-10 17:52:38 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/** proxy type: HTTP, SOCKS5 */
+$PAC_PROXYTYPE = "SOCKS5";
+
+/** proxy server and port */
+$PAC_PROXY = "PROXY_SERVER:PROXY_PORT";
+
+/** direct proxy type: HTTP, SOCKS5. If $PAC_DIRECT = "DIRECT", this option is not available. */
+$PAC_DIRECTTYPE = "SOCKS5";
+
+/** direct setting, default value: DIRECT */
+$PAC_DIRECT = "DIRECT";
+?>
