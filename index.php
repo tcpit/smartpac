31b8885830839966f6651754216d0dc77491fe63
TCP <tcpit@users.noreply.github.com>
2016-07-05 09:59:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $PAC_TEMPLATE_ALL = <<<PAC_TEMPLATE_ALL
 var proxy = "%1\$s; DIRECT";
 function FindProxyForURL(url, host) {
     var isIpV4Addr = /^(\d+.){3}\d+$/;
-    if(isPlainHostName(host) || shExpMatch(host, "localhost") || shExpMatch(host, "*.local") ||
+    if(isPlainHostName(host) || shExpMatch(host, "localhost") || shExpMatch(host, "*.local") || shExpMatch(host, "google-analytics.com") || 
     (isIpV4Addr.test(host) && (shExpMatch(host, "10.*") || shExpMatch(host, "127.*") || shExpMatch(host, "192.168.*") || shExpMatch(host, "172.16.*") || shExpMatch(host, "172.17.*") || shExpMatch(host, "172.18.*") || shExpMatch(host, "172.19.*") || shExpMatch(host, "172.20.*") || shExpMatch(host, "172.21.*") || shExpMatch(host, "172.22.*") || shExpMatch(host, "172.23.*") || shExpMatch(host, "172.24.*") || shExpMatch(host, "172.25.*") || shExpMatch(host, "172.26.*") || shExpMatch(host, "172.27.*") || shExpMatch(host, "172.28.*") || shExpMatch(host, "172.29.*") || shExpMatch(host, "172.30.*") || shExpMatch(host, "172.31.*")))){
 		return "DIRECT";
     }
