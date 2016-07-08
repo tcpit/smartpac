71bc94cf271706e13b4c16cb90758d2372702ed2
TCP <tcpit@users.noreply.github.com>
2016-07-08 14:10:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ function FindProxyForURL(url, host) {
     (isIpV4Addr.test(host) && (shExpMatch(host, "10.*") || shExpMatch(host, "127.*") || shExpMatch(host, "192.168.*") || shExpMatch(host, "172.16.*") || shExpMatch(host, "172.17.*") || shExpMatch(host, "172.18.*") || shExpMatch(host, "172.19.*") || shExpMatch(host, "172.20.*") || shExpMatch(host, "172.21.*") || shExpMatch(host, "172.22.*") || shExpMatch(host, "172.23.*") || shExpMatch(host, "172.24.*") || shExpMatch(host, "172.25.*") || shExpMatch(host, "172.26.*") || shExpMatch(host, "172.27.*") || shExpMatch(host, "172.28.*") || shExpMatch(host, "172.29.*") || shExpMatch(host, "172.30.*") || shExpMatch(host, "172.31.*")))){
 		return "DIRECT";
     }
-    if(shExpMatch(host, "*google*.*") || shExpMatch(host, "*blogspot.*"))
+    if(shExpMatch(host, "*google.*") || shExpMatch(host, "*blogspot.*"))
     	return proxy;
     var suffix;
     var pos = host.lastIndexOf(".");
